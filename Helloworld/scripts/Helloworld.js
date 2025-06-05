77bef668f2f534929073a038b4cb0a1c05e392a1
harshinibekkam <166860566+harshinibekkam@users.noreply.github.com>
2025-06-05 15:54:26 +0530

    Add files via upload

---
+++ Helloworld/scripts/Helloworld.js
@@ -0,0 +1,9 @@
+define("DS/Helloworld/scripts/Helloworld",[], function(){
+    var myWidget={
+        onLoadWidget: function(){
+            widget.body.innerHTML="<div class='main-container'><p>hello, world Harshi!</p></div>";
+
+        },
+    };
+    return myWidget;
+    });
