b06d6c2ccc87ca8dd29512f3ecf6c5b564a92da3
harshinibekkam <166860566+harshinibekkam@users.noreply.github.com>
2025-06-05 16:16:51 +0530

    Update Helloworld.js

--- Helloworld/scripts/Helloworld.js
+++ Helloworld/scripts/Helloworld.js
@@ -1,7 +1,7 @@
 define("DS/Helloworld/scripts/Helloworld",[], function(){
     var myWidget={
         onLoad: function(){
-            widget.body.innerHTML="<div class='main-container'><p>hello, world Harshiiii!</p></div>";
+            widget.body.innerHTML="<div class='main-container'><p>hello, world Harshiii!</p></div>";
 
         },
     };
