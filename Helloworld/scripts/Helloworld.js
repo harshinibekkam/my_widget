1d01111cdea71e72015f33bdcaaf85132c054a60
harshinibekkam <166860566+harshinibekkam@users.noreply.github.com>
2025-06-06 12:03:26 +0530

    Update Helloworld.js

--- Helloworld/scripts/Helloworld.js
+++ Helloworld/scripts/Helloworld.js
@@ -1,10 +1,13 @@
-define("DS/Helloworld/scripts/Helloworld",[], function(){
+ define("DS/Helloworld/scripts/Helloworld",[], function(){
     var myWidget={
         onLoad: function(){
+            
+        var type = widget.getValue("Type");
+        alert("type::"+type);
             widget.body.innerHTML="<div class='main-container'><p>helloooooooooo!</p></div>";
 
         },
     };
-   
+   // return myWidget;
  widget.addEvent('onLoad',myWidget.onLoad);
     });
