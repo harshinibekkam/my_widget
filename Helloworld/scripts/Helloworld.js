72e76b7b57593397488b49fe768f562d2c00acb0
harshinibekkam <166860566+harshinibekkam@users.noreply.github.com>
2025-06-06 18:30:28 +0530

    Update Helloworld.js

--- Helloworld/scripts/Helloworld.js
+++ Helloworld/scripts/Helloworld.js
@@ -6,18 +6,41 @@
             var name = widget.getValue("Name");
             var revision = widget.getValue("Revision");
 
-            /* Optional: alert for debugging
-            alert("Type: " + type);
-            alert("Name: " + name);
-            alert("Revision: " + revision);*/
-
-            // Display values in the widget body
+            // Build HTML content with a form
             widget.body.innerHTML = 
                 "<div class='main-container'>" +
                     "<p><strong>Type:</strong> " + type + "</p>" +
                     "<p><strong>Name:</strong> " + name + "</p>" +
                     "<p><strong>Revision:</strong> " + revision + "</p>" +
+                    "<hr/>" +
+                    "<form id='myForm'>" +
+                        "<label for='input1'>Enter some text:</label><br>" +
+                        "<input type='text' id='input1' name='input1' placeholder='Type here...' /><br><br>" +
+
+                        "<label for='select1'>Choose an option:</label><br>" +
+                        "<select id='select1' name='select1'>" +
+                            "<option value='option1'>Option 1</option>" +
+                            "<option value='option2'>Option 2</option>" +
+                        "</select><br><br>" +
+
+                        "<input type='submit' value='Submit' />" +
+                    "</form>" +
+
+                    "<div id='formResult' style='margin-top: 10px;'></div>" +
                 "</div>";
+
+            // Handle form submission
+            document.getElementById("myForm").addEventListener("submit", function(event) {
+                event.preventDefault(); // Prevent page reload
+
+                var inputText = document.getElementById("input1").value;
+                var selectedOption = document.getElementById("select1").value;
+
+                // Display the result
+                document.getElementById("formResult").innerHTML = 
+                    "<p><strong>You entered:</strong> " + inputText + "</p>" +
+                    "<p><strong>You selected:</strong> " + selectedOption + "</p>";
+            });
         },
     };
 
