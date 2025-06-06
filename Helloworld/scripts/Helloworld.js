8c578af6feb925510d9cd7876aa8b4a696114dfe
harshinibekkam <166860566+harshinibekkam@users.noreply.github.com>
2025-06-06 16:37:31 +0530

    Update Helloworld.js

--- Helloworld/scripts/Helloworld.js
+++ Helloworld/scripts/Helloworld.js
@@ -1,17 +1,27 @@
- define("DS/Helloworld/scripts/Helloworld",[], function(){
-    var myWidget={
+ define("DS/Helloworld/scripts/Helloworld", [], function(){
+    var myWidget = {
         onLoad: function(){
-        widget.setValue("Type","Pen-type");    
-        var type = widget.getValue("Type");
-      //  alert("type::"+type);
-         var type = widget.getValue("Name");
-       // alert("type::"+type);
-         var type = widget.getValue("Revision");
-       // alert("type::"+type);
-            widget.body.innerHTML="<div class='main-container'><p>helloooooooooo!</p></div>";
+            // Get values from widget preferences
+            var type = widget.getValue("Type");
+            var name = widget.getValue("Name");
+            var revision = widget.getValue("Revision");
 
+            // Optional: alert for debugging
+            alert("Type: " + type);
+            alert("Name: " + name);
+            alert("Revision: " + revision);
+
+            // Display values in the widget body
+            widget.body.innerHTML = `
+                <div class='main-container'>
+                    <p><strong>Type:</strong> ${type}</p>
+                    <p><strong>Name:</strong> ${name}</p>
+                    <p><strong>Revision:</strong> ${revision}</p>
+                </div>
+            `;
         },
     };
-   // return myWidget;
- widget.addEvent('onLoad',myWidget.onLoad);
-    });
+
+    // Register the onLoad function
+    widget.addEvent('onLoad', myWidget.onLoad);
+});
